22ae1ebc1c802944d79f844329a11ec0d6c91976
ermias-mangisto <ermiasmangisto@gmail.com>
2021-09-29 06:50:27 +0300

    first commit

---
+++ javascript/cart.js
@@ -0,0 +1,45 @@
+let cart=[
+    {name:"The Spook's Secret"   ,
+    id: 14  ,
+    price:17 ,
+    description:"the nights are drawing in and it's time for Tom and his master to move to Anglezarke",
+ category:"fantasy" ,  
+imgs: ["/media/sa3(1).jpg","/media/sa3(2).jpg"]   },
+    {name:"the return of the king"   ,
+    id: 15  ,
+    price:15 ,
+    description:"One Ring to rule them all, One Ring to find them, One Ring to bring them all and in the darkness bind them",
+ category:"fantasy" ,  
+imgs: ["/media/lotr3(1).jfif","/media/lotr3(2).jfif"]   },
+    {name:"Dracula"   ,
+    id: 16  ,
+    price:15 ,
+    description:"The original world classic horror novel Dracula",
+ category:"fantasy" ,  
+imgs: ["/media/drac1(1).jfif","/media/drac1(2).jfif"]   },
+]
+let sum=0;
+let revBtn=document.getElementsByClassName("revBtn")
+let row=document.getElementsByClassName("tableRow")  
+let counter=0;
+
+      for(let i=0;i<cart.length;i++){
+    cartTable.innerHTML+= ` <tr class="tableRow">
+    <td><img width="100px" height="100px" src=${cart[i].imgs[0]}>  ${cart[i].name}</td> 
+      <td>price:${cart[i].price}$</td>
+      <td><button class="revBtn">remove form cart</button></td>
+      </tr>`}
+         for (let i = 0; i < cart.length; i++) {
+                sum+=cart[i].price;
+                total.innerHTML=`<p>Total Price:${sum}$<p>`
+
+            }
+        for(let i=0;i<cart.length;i++){
+            revBtn[i].addEventListener("click",()=>{
+             cart.splice(i,1)
+                 row[i].innerHTML=` `;
+             total.innerHTML=`<p>Total Price:${sum-=cart[i].price}$<p>`;
+             
+            })}
+ 
+  
---
+++ javascript/home.js
@@ -0,0 +1,16 @@
+for(let i=0;i<5;i++){
+    rec.innerHTML+=`<div>
+      <p>${books[i].name}</p>
+      <img src=${books[i].imgs[0]}>
+      <p>price:${books[i].price}$</p>
+      <p>${books[i].description}</p>
+      <button class="addBtn"> Add to cart </button>
+      </div>`}
+   
+      let cart=[];
+      let add=document.getElementsByClassName("addBtn");
+  for(let i=0;i<books.length;i++){
+      add[i].addEventListener("click",()=>{
+       cart.push(books[i])
+   console.log(cart);
+      })}
